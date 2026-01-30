751de2a25df9a4c6aa7b629e98b0d4c80a08deee
jayavibhavnk <jaya11vibhav@gmail.com>
2026-01-30 09:15:00 -0800

    feat: public exports

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export type { MemoryEntry, MemoryRole } from "./types.js";
+export { SharedMemory, type MemoryListener } from "./memory.js";
+export { AssemblyRegistry } from "./registry.js";
+export { appendFromFuseMessage, mergePrompt } from "./fuse.js";
